06ec72d5e35e3f1d2ac84381ceb77a0960f41bd3
S.Bobrischev <sergei.bobrischev@exyte.com>
2021-11-03 01:12:29 +0700

    Create 'Player Screen State' holder

--- app/src/main/java/com/example/composesample/PlayerScreen.kt
+++ app/src/main/java/com/example/composesample/PlayerScreen.kt
@@ -16,6 +16,8 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -51,91 +53,125 @@ enum class ToNowPlaying {
     None, Stable, Forward, Backward,
 }
 
+class PlayerScreenState(
+    constraints: Constraints,
+    private val density: Density,
+    isInPreviewMode: Boolean,
+) {
+    @Stable
+    private fun Float.toDp() = this.toDp(density)
+    @Stable
+    private fun Int.toDp() = this.toDp(density)
+
+    private val easing = FastOutLinearInEasing
+
+    var currentScreen by mutableStateOf(Screen.Player)
+
+    var currentDragOffset by mutableStateOf(0f)
+    var playerControlHeight by mutableStateOf(0)
+    var songInfoHeight by mutableStateOf(0)
+
+    val maxContentWidth = constraints.maxWidth.toFloat()
+    val maxContentHeight = constraints.maxHeight.toFloat()
+
+    val backHandlerEnabled by derivedStateOf { currentScreen != Screen.Player }
+
+    val fromPlayerControlsToAlbumsListProgress by derivedStateOf {
+        if (isInPreviewMode) {
+            0f
+        } else {
+            currentDragOffset / maxContentWidth
+        }
+    }
+
+    val playerControlOffset by derivedStateOf {
+        -(playerControlHeight * fromPlayerControlsToAlbumsListProgress * 1.33f).toDp()
+    }
+    val songInfoTopPadding by derivedStateOf {
+        lerpF(
+            playerControlHeight.toFloat(),
+            0f,
+            (1f - fromPlayerControlsToAlbumsListProgress)
+        ).toDp()
+    }
+
+    val albumsInfoSize by derivedStateOf { (maxContentHeight * 0.4f).toDp() }
+
+    val photoScale by derivedStateOf {
+        easing.transform(
+            lerpF(1f,
+                1.3f,
+                fromPlayerControlsToAlbumsListProgress)
+        )
+    }
+    val commentsListOffset by derivedStateOf {
+        -(maxContentWidth * (1f - fromPlayerControlsToAlbumsListProgress)).toDp()
+    }
+
+    val songInfoOffset by derivedStateOf {
+        if (fromPlayerControlsToAlbumsListProgress < 0.25f) {
+            0.dp
+        } else {
+            val progress =
+                easing.transform((1f - fromPlayerControlsToAlbumsListProgress) / 0.75f)
+            val progress1 =
+                min(1f, easing.transform((1.5f - fromPlayerControlsToAlbumsListProgress)))
+            val off = lerpF(
+                0f,
+                playerControlHeight.toFloat(),
+                progress
+            )
+            val toff = (songInfoHeight - lerpF(
+                0f,
+                songInfoHeight.toFloat(),
+                progress1
+            ))
+            -(playerControlHeight.toFloat() - off + toff).toDp()
+        }
+    }
+}
+
+@Composable
+fun rememberPlayerScreenState(
+    constraints: Constraints,
+    density: Density = LocalDensity.current,
+    isInPreviewMode: Boolean = LocalPreviewMode.current,
+) = remember(constraints) {
+    PlayerScreenState(
+        constraints,
+        density,
+        isInPreviewMode
+    )
+}
+
 @Composable
 fun PlayerScreen(playbackData: PlaybackData = PlaybackData()) = ProvideWindowInsets {
     BoxWithConstraints {
-        val isInPreviewMode = LocalPreviewMode.current
+        val sharedElementTargetSize = 230.dp
+        val draggableButtonSize = DpSize(width = 64.dp, height = 48.dp)
+
         val density = LocalDensity.current
-        val maxContentWidth = constraints.maxWidth.toFloat()
-        val maxContentHeight = constraints.maxHeight.toFloat()
 
         val insets = LocalWindowInsets.current
         val bottomInset by derivedStateOf { insets.navigationBars.bottom.toDp(density) }
         val topInset by derivedStateOf { insets.statusBars.top.toDp(density) }
 
-        fun Float.toDp() = this.toDp(density)
-        fun Int.toDp() = this.toDp(density)
+        val screenState = rememberPlayerScreenState(constraints)
 
-        val sharedElementTargetSize = 230.dp
-        val draggableButtonSize = DpSize(width = 64.dp, height = 48.dp)
-        var currentScreen by remember { mutableStateOf(Screen.Player) }
-        val backHandlerEnabled by derivedStateOf { currentScreen != Screen.Player }
         var toNowPlayingTransition by remember { mutableStateOf(ToNowPlaying.None) }
-        var currentDragOffset by remember { mutableStateOf(0f) }
-        var playerControlHeight by remember { mutableStateOf(0) }
-        var songInfoHeight by remember { mutableStateOf(0) }
-        val fromPlayerControlsToAlbumsListProgress by derivedStateOf {
-            if (isInPreviewMode) {
-                0f
-            } else {
-                currentDragOffset / maxContentWidth
-            }
-        }
-        val playerControlOffset by derivedStateOf {
-            -(playerControlHeight * fromPlayerControlsToAlbumsListProgress * 1.33f).toDp()
-        }
-        val songInfoTopPadding by derivedStateOf {
-            lerpF(
-                playerControlHeight.toFloat(),
-                0f,
-                (1f - fromPlayerControlsToAlbumsListProgress)
-            ).toDp()
-        }
-        val easing = FastOutLinearInEasing
 
-        val songInfoOffset by derivedStateOf {
-            if (fromPlayerControlsToAlbumsListProgress < 0.25f) {
-                0.dp
-            } else {
-                val progress =
-                    easing.transform((1f - fromPlayerControlsToAlbumsListProgress) / 0.75f)
-                val progress1 =
-                    min(1f, easing.transform((1.5f - fromPlayerControlsToAlbumsListProgress)))
-                val off = lerpF(
-                    0f,
-                    playerControlHeight.toFloat(),
-                    progress
-                )
-                val toff = (songInfoHeight - lerpF(
-                    0f,
-                    songInfoHeight.toFloat(),
-                    progress1
-                ))
-                -(playerControlHeight.toFloat() - off + toff).toDp()
-            }
-        }
-        val albumsInfoSize by derivedStateOf { (maxContentHeight * 0.4f).toDp() }
-
-        val photoScale by derivedStateOf {
-            easing.transform(lerpF(1f,
-                1.3f,
-                fromPlayerControlsToAlbumsListProgress))
-        }
-        val commentsListOffset by derivedStateOf {
-            -(maxContentWidth * (1f - fromPlayerControlsToAlbumsListProgress)).toDp()
-        }
         val animScope = rememberCoroutineScope()
         val commentsScrollState = rememberScrollState()
 
         var transitioned by remember { mutableStateOf(false) }
 
-        var sharedElementInitialData by remember { mutableStateOf(SharedElementData.NONE) }
+        var sharedElementParams by remember { mutableStateOf(SharedElementData.NONE) }
         val titleProgressForward = remember { Animatable(0f) }
         val sharedProgress by derivedStateOf { titleProgressForward.value }
 
         fun animateOffset(initialValue: Float, targetValue: Float, onEnd: () -> Unit) {
             val distance = abs(targetValue - initialValue)
-            val distancePercent = distance / maxContentWidth
+            val distancePercent = distance / screenState.maxContentWidth
             val duration = (250 * distancePercent).toInt()
 
             animScope.launch {
@@ -143,29 +179,28 @@ fun PlayerScreen(playbackData: PlaybackData = PlaybackData()) = ProvideWindowIns
                     initialValue = initialValue,
                     targetValue = targetValue,
                     animationSpec = tween(duration),
-                ) { value, _ -> currentDragOffset = value }
+                ) { value, _ -> screenState.currentDragOffset = value }
                 onEnd()
             }
         }
 
         fun expand() {
             animateOffset(
-                initialValue = currentDragOffset,
-                targetValue = maxContentWidth
+                initialValue = screenState.currentDragOffset,
+                targetValue = screenState.maxContentWidth
             ) {
-                currentScreen = Screen.Comments
+                screenState.currentScreen = Screen.Comments
             }
         }
 
         fun collapse() {
             animateOffset(
-                initialValue = currentDragOffset,
+                initialValue = screenState.currentDragOffset,
                 targetValue = 0f
             ) {
-                currentScreen = Screen.Player
+                screenState.currentScreen = Screen.Player
             }
         }
-
 
         ConstraintLayout(modifier = Modifier
             .fillMaxSize()
@@ -173,27 +208,27 @@ fun PlayerScreen(playbackData: PlaybackData = PlaybackData()) = ProvideWindowIns
         ) {
             val (control, statsInfo, albumInfo, commentsList, albumsList, draggableButton) = createRefs()
 
-            if (currentScreen != Screen.Comments) {
+            if (screenState.currentScreen != Screen.Comments) {
                 AlbumInfoContainer(
                     modifier = Modifier
                         .constrainAs(albumInfo) {
                             bottom.linkTo(parent.bottom)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
-                            height = Dimension.value(albumsInfoSize)
+                            height = Dimension.value(screenState.albumsInfoSize)
                         },
-                    photoScale = photoScale,
+                    photoScale = screenState.photoScale,
                 )
             }
 
-            if (currentScreen != Screen.Player) {
+            if (screenState.currentScreen != Screen.Player) {
                 Box(
                     modifier = Modifier
                         .shadow(elevation = 2.dp)
                         .constrainAs(commentsList) {
                             top.linkTo(albumsList.bottom)
                             bottom.linkTo(parent.bottom)
-                            start.linkTo(parent.start, margin = commentsListOffset)
+                            start.linkTo(parent.start, margin = screenState.commentsListOffset)
                             height = Dimension.fillToConstraints
                         }
                 ) {
@@ -214,17 +249,17 @@ fun PlayerScreen(playbackData: PlaybackData = PlaybackData()) = ProvideWindowIns
                     topPadding = topInset + 24.dp,
                     albumData = playbackData.albums,
                     transitionAnimationProgress = sharedProgress,
-                    appearingAnimationProgress = fromPlayerControlsToAlbumsListProgress,
+                    appearingAnimationProgress = screenState.fromPlayerControlsToAlbumsListProgress,
                     onBackClick = {
-                        currentScreen = Screen.TransitionToComments
+                        screenState.currentScreen = Screen.TransitionToComments
                         collapse()
                     },
                     onInfoClick = { data, x, y, size ->
-                        sharedElementInitialData = SharedElementData(
+                        sharedElementParams = SharedElementData(
                             data,
-                            x.toDp(),
-                            y.toDp(),
-                            size.toDp()
+                            x.toDp(density),
+                            y.toDp(density),
+                            size.toDp(density)
                         )
                         transitioned = true
                         toNowPlayingTransition = ToNowPlaying.Stable
@@ -238,24 +273,24 @@ fun PlayerScreen(playbackData: PlaybackData = PlaybackData()) = ProvideWindowIns
                 )
             }
 
-            if (currentScreen != Screen.Comments) {
+            if (screenState.currentScreen != Screen.Comments) {
                 SongInfoContainer(
                     modifier = Modifier
-                        .onSizeChanged { (_, h) -> songInfoHeight = h }
+                        .onSizeChanged { (_, h) -> screenState.songInfoHeight = h }
                         .constrainAs(statsInfo) {
-                            top.linkTo(control.bottom, margin = songInfoOffset - 48.dp)
+                            top.linkTo(control.bottom, margin = screenState.songInfoOffset - 48.dp)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
                             height = Dimension.wrapContent
                         },
-                    topPadding = songInfoTopPadding,
+                    topPadding = screenState.songInfoTopPadding,
                 )
 
                 PlayerControlContainer(
                     modifier = Modifier
-                        .onSizeChanged { (_, h) -> playerControlHeight = h }
+                        .onSizeChanged { (_, h) -> screenState.playerControlHeight = h }
                         .constrainAs(control) {
-                            top.linkTo(parent.top, margin = playerControlOffset)
+                            top.linkTo(parent.top, margin = screenState.playerControlOffset)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
                             height = Dimension.wrapContent
@@ -266,30 +301,32 @@ fun PlayerScreen(playbackData: PlaybackData = PlaybackData()) = ProvideWindowIns
                 DraggableButton(
                     modifier = Modifier
                         .constrainAs(draggableButton) {
-                            start.linkTo(parent.start, margin = currentDragOffset.toDp())
+                            start.linkTo(parent.start,
+                                margin = screenState.currentDragOffset.toDp(density))
                             bottom.linkTo(parent.bottom, margin = 64.dp)
                         }
                         .size(draggableButtonSize),
                     onPointerDown = {
                         animScope.coroutineContext.cancelChildren()
-                        currentScreen = Screen.TransitionToComments
+                        screenState.currentScreen = Screen.TransitionToComments
                     },
                     onClick = {
                         animScope.coroutineContext.cancelChildren()
-                        currentScreen = Screen.TransitionToComments
+                        screenState.currentScreen = Screen.TransitionToComments
                         expand()
                     },
                     onOffsetChange = { dragAmount ->
                         val newOffset = minOf(
-                            currentDragOffset + dragAmount,
-                            (maxContentWidth - draggableButtonSize.width.toPx(density))
+                            screenState.currentDragOffset + dragAmount,
+                            (screenState.maxContentWidth - draggableButtonSize.width.toPx(density))
                         )
                         if (newOffset >= 0) {
-                            currentDragOffset = newOffset
+                            screenState.currentDragOffset = newOffset
                         }
                     },
                     onDragFinished = {
-                        val shouldExpand = currentDragOffset > maxContentWidth / 2f
+                        val shouldExpand =
+                            screenState.currentDragOffset > screenState.maxContentWidth / 2f
                         if (shouldExpand) {
                             expand()
                         } else {
@@ -311,18 +348,19 @@ fun PlayerScreen(playbackData: PlaybackData = PlaybackData()) = ProvideWindowIns
 
             if (toNowPlayingTransition == ToNowPlaying.Stable) {
                 NowPlayingScreen(
-                    albumInfo = sharedElementInitialData.albumInfo,
+                    albumInfo = sharedElementParams.albumInfo,
                     isAppearing = transitioned,
                     sharedElementParams = SharedElementParams(
                         initialOffset = Offset(
-                            sharedElementInitialData.offsetX.toPx(density).toFloat(),
-                            sharedElementInitialData.offsetY.toPx(density).toFloat(),
+                            sharedElementParams.offsetX.toPx(density).toFloat(),
+                            sharedElementParams.offsetY.toPx(density).toFloat(),
                         ),
                         targetOffset = Offset(
-                            x = maxContentWidth / 2f - sharedElementTargetSize.toPx(density) / 2f,
+                            x = screenState.maxContentWidth / 2f - sharedElementTargetSize.toPx(
+                                density) / 2f,
                             y = 50.dp.toPx(density).toFloat()
                         ),
-                        initialSize = sharedElementInitialData.size,
+                        initialSize = sharedElementParams.size,
                         targetSize = sharedElementTargetSize,
                         initialCornerRadius = 10.dp,
                         targetCornerRadius = sharedElementTargetSize / 2
@@ -339,11 +377,11 @@ fun PlayerScreen(playbackData: PlaybackData = PlaybackData()) = ProvideWindowIns
                 )
             }
 
-            BackHandler(backHandlerEnabled) {
+            BackHandler(screenState.backHandlerEnabled) {
                 when {
                     transitioned -> goBackFromNowPlayingScreen()
-                    currentScreen != Screen.Player -> {
-                        currentScreen = Screen.TransitionToComments
+                    screenState.currentScreen != Screen.Player -> {
+                        screenState.currentScreen = Screen.TransitionToComments
                         collapse()
                     }
                 }
